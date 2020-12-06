d3e21a69996a5dfa207458f24f59a906ee1080fc
Sabina-Nakarmi <sabinanakarm6@gmail.com>
2020-12-06 10:33:27 +0545

    Initial commit

---
+++ src/com/Loading/Main.java
@@ -0,0 +1,12 @@
+package com.Loading;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[][] numbers={{1,2,3 },{4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
+
+    }
+}
